9795ba84b993a3ca9c9b303ae12ce5bf697522ea
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-11-24 15:21:34 +0200

    creates Factory pattern

---
+++ src/creational/patterns/factory/CheeseBurger.java
@@ -0,0 +1,9 @@
+package creational.patterns.factory;
+
+public class CheeseBurger extends Hamburger {
+    public CheeseBurger() {
+        setName("Normal Cheese");
+        setBuns("Cookie dough buns!");
+        setSauce("Sauce");
+    }
+}
---
+++ src/creational/patterns/factory/VeggieBurger.java
@@ -0,0 +1,10 @@
+package creational.patterns.factory;
+
+public class VeggieBurger extends Hamburger {
+    public VeggieBurger() {
+
+        setName("Normal Veggie");
+        setBuns("Cookie dough buns!");
+        setSauce("Sauce");
+    }
+}
